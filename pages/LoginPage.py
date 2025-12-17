7bbf8003623f8064c98074b3f1255c21dccbaf4c
jiwajiw <fjijwjij@gmail.com>
2025-12-17 21:23:32 +0500

    test_go_to_recovery_after_fails is added

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,5 +1,8 @@
 import allure
 from pages.BasePage import BasePage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
 class LoginPageLocators(BasePage):
@@ -11,12 +14,14 @@ class LoginPageLocators(BasePage):
     QR_BUTTON = (By.XPATH, '//button[.//span[text()="Войти по QR-коду"]]')
     CAN_NOT_ENTER_BUTTON = (By.XPATH, '//button[@aria-label="Не получается войти?"]')
     REGISTRATION_BUTTON = (By.XPATH, '//button[.//span[text()="Зарегистрироваться"]]')
+    RESTORE_LINK = (By.XPATH, '//a[contains(@href, "anonymRecoveryStart")]')
     VK_LINK = (By.XPATH, '//a[@data-l="t,vkc"]')
     MAILRU_LINK = (By.XPATH, '//a[@data-l="t,mailru"]')
     GOOGLE_LINK = (By.XPATH, '//a[@data-l="t,google"]')
     YANDEX_LINK = (By.XPATH, '//a[@data-l="t,yandex"]')
     APPLE_LINK = (By.XPATH, '//a[@data-l="t,apple"]')
     ERROR_TEXT = (By.CSS_SELECTOR, "span[class*='LoginForm-module__error']")
+    GO_BACK_BUTTON = (By.XPATH, '//button[.//span[text()="Отмена"]]')
 
 class LoginPageHelper(BasePage):
     def __init__(self, driver):
@@ -24,6 +29,8 @@ class LoginPageHelper(BasePage):
         self.check_page()
 
     def check_page(self):
+        with allure.step('Проверить корректность загрузки страницы'):
+            self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_TAB)
         self.find_element(LoginPageLocators.QR_TAB)
         self.find_element(LoginPageLocators.LOGIN_FIELD)
@@ -38,12 +45,12 @@ class LoginPageHelper(BasePage):
         self.find_element(LoginPageLocators.YANDEX_LINK)
         self.find_element(LoginPageLocators.APPLE_LINK)
 
-    @allure.step('Нажать на кнпоку "Войти"')
+    @allure.step('Нажать на кнопку "Войти"')
     def click_login(self):
         self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
 
-    @allure.step('Получать текст ошибки')
+    @allure.step('Получить текст ошибки')
     def get_error_text(self):
         self.attach_screenshot()
         return self.find_element(LoginPageLocators.ERROR_TEXT).text
@@ -52,8 +59,36 @@ class LoginPageHelper(BasePage):
     def enter_login(self, login):
         login_field = self.find_element(LoginPageLocators.LOGIN_FIELD)
         login_field.send_keys(login)
+        self.attach_screenshot()
 
     @allure.step('Ввести пароль')
-    def enter_password_field(self, password):
+    def enter_password(self, password):
         password_field = self.find_element(LoginPageLocators.PASSWORD_FIELD)
         password_field.send_keys(password)
+        self.attach_screenshot()
+
+    @allure.step('Перейти к восстановлению')
+    def click_recovery(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.RESTORE_LINK).click()
+
+    @allure.step('Очистить поле логина')
+    def clear_login(self):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).clear()
+
+    @allure.step('Очистить поле пароля')
+    def clear_password(self):
+        self.find_element(LoginPageLocators.PASSWORD_FIELD).clear()
+
+    @allure.step('Дождаться ошибки авторизации')
+    def wait_error_optional(self, time=5):
+        try:
+            return WebDriverWait(self.driver, time).until(
+                expected_conditions.presence_of_element_located(LoginPageLocators.ERROR_TEXT)
+            )
+        except TimeoutException:
+            return None
+
+    @allure.step('Дождаться появления кнопки восстановления')
+    def wait_recovery(self):
+        return WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(LoginPageLocators.RESTORE_LINK))
