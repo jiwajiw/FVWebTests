ea3caf2543f16478e9e02eb5936325c7ff194058
jiwajiw <fjijwjij@gmail.com>
2025-12-22 21:12:28 +0500

    test_help_test is added

---
+++ tests/HelpTests.py
@@ -0,0 +1,17 @@
+import allure
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.HelpPage import HelpPageHelper, HelpPageLocators
+from pages.AdvertisementCabinetHelp import AdvertisementCabinetHelpHelper
+
+BASE_URL = 'https://ok.ru/help'
+
+@allure.suite('Проверка страницы "помощь"')
+@allure.title('Проверка ошибки при пустой форме пароля')
+def test_help_test(browser):
+    BasePage(browser).get_url(BASE_URL)
+    HelpPage = HelpPageHelper(browser)
+    HelpPage.scrollToitem(HelpPageLocators.ADVERTISEMENT_CABINET)
+    AdCabinetPage = HelpPageHelper(browser)
+    AdCabinetPage.click_ad_cabinet()
+    AdvertisementCabinetHelpHelper(browser)
