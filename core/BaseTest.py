21aa24d9c593262ae489abeffad409f101d67587
jiwajiw <fjijwjij@gmail.com>
2026-01-12 20:17:35 +0500

    browser in BaseTest was changes

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -6,7 +6,7 @@ def browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=ru")
     driver = webdriver.Remote(
-        command_executor="http://localhost:8080",
+        command_executor="http://213.176.118.218:4444",
         options=options
     )
     driver.set_window_size(1920, 1080)
